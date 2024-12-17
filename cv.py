face148383043c15ee1ad3c7b1f68fbe0c47f2ed
Maxime Franc <maxime.franc1@gmail.com>
2024-12-17 13:12:17 +0100

    add tailwind to my cv flask app

--- cv.py
+++ cv.py
@@ -1,7 +1,17 @@
 from flask import Flask
+from flask import url_for
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def index():
+    return "<p>Site de maxime Franc!</p>"
+
+@app.route("/cv")
+def cv():
+    return render_template('cv.html')
+
+with app.test_request_context():
+    print(url_for('index'))
+    print(url_for('cv'))
